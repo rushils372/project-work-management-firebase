2f3cee0079f99363a00580355551fc0d0203c141
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-15 16:21:23 +0530

    DIP1

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase"
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyB-HJbe8Z9mZTC7kymoP4fbONlzxPI8Bko",
@@ -17,8 +18,9 @@ const firebaseConfig = {
   // Init Services
   const projectFirestore = firebase.firestore()
   const projectAuth = firebase.auth()
+  const projectStorage = firebase.storage()
 
   // Timestamp
   const timestamp = firebase.firestore.Timestamp
 
-  export {projectFirestore, projectAuth, timestamp}
+  export {projectFirestore, projectAuth, timestamp, projectStorage}
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -9,9 +9,41 @@ export default function Signup() {
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, displayName, thumbnail);
+    
+  }
+
+  const handleFileChange = (e) =>{
+    setThumbnail(null)
+    let selected = e.target.files[0]
+    console.log(selected);
+    
+    if (!selected) {
+      setThumbnailError('Please select a file')
+      return
+    }
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image')
+      return
+    }
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb')
+      return
+    }
+
+    setThumbnailError(null)
+    setThumbnail(selected)
+    console.log('Thumbnail Updated');
+    
+
+  }
 
   return (
-    <form className='auth-form'>
+    <form className='auth-form' onSubmit={handleSubmit}>
       <h2>Sign Up</h2>
       <label>
         <span>Email :</span>
@@ -45,7 +77,9 @@ export default function Signup() {
         <input
           required
           type="file"
+          onChange={handleFileChange}
         />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
       <button className="btn">Sign Up</button>
     </form>
