a7260312ccc1493dd6dd45cb4cc546c2d5126fbb
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-18 17:27:03 +0530

    DIP7

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -2,6 +2,10 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import Select from 'react-select'
 import { useCollection } from '../../hooks/useCollection'
+import { timestamp } from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useHistory } from 'react-router-dom'
 
 // Styles
 import './Create.css'
@@ -14,8 +18,11 @@ const categories = [
 ]
 
 export default function Create() {
+  const history = useHistory()
+  const { addDocument, response } = useFirestore('projects')
   const { documents } = useCollection('users')
   const [users, setUsers] = useState([])
+  const { user } = useAuthContext()
 
   // form field values
   const [name, setName] = useState('')
@@ -48,7 +55,34 @@ export default function Create() {
       return
     }
 
-    console.log(name, details, dueDate, category.value, assignedUsers)
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid
+    }
+
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id
+      }
+    })
+
+    const project = {
+      name,
+      details,
+      category : category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      assignedUsersList, 
+      createdBy,
+      comments: []
+    }
+
+    await addDocument(project)
+    if(!response.error) {
+      history.push('/')
+    } 
   }
 
   return (
