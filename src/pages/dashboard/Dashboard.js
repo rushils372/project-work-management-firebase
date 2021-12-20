24341d8c63555aa13d122592c0340f441de0b64a
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-20 18:49:50 +0530

    DIP10

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,18 +1,26 @@
 import { useCollection } from '../../hooks/useCollection'
+import { useState  } from 'react'
 
 // components
 import ProjectList from '../../components/ProjectList'
+import ProjectFilter from './ProjectFilter'
 
 // Styles
 import './Dashboard.css'
 
 export default function Dashboard() {
   const { documents, error} = useCollection('projects')
+  const [filter, setFilter] = useState('all')
+
+  const changeFilter = (newFilter) => {
+    setFilter(newFilter)
+  }
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter changeFilter={changeFilter} />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
